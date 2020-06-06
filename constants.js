8f7c3c8a7629c2f2b69d5c03e4577a45e3a81526
Michael Danilov <michael@danilov.me>
2020-06-06 09:56:38 +0300

    fix double slash in yatranslate lint (#3)

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const YANDEX_TRANSLATE_API_URL = 'https://translate.yandex.net/api/v1.5/tr.json';
-const YANDEX_TRANSLATE_URL = 'https://translate.yandex.com/';
+const YANDEX_TRANSLATE_URL = 'https://translate.yandex.com';
 
 module.exports = {
   YANDEX_TRANSLATE_API_URL,
